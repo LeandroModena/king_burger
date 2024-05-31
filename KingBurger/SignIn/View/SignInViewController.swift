0568692da1e2d53f1ec28bc0d89cc37ee6be802f
Leandro <modena@outlook.com>
2024-05-31 13:28:53 -0300

    push m

--- KingBurger/SignIn/View/SignInViewController.swift
+++ KingBurger/SignIn/View/SignInViewController.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+import UIKit
+
+class SignInViewController: UIViewController, SignInViewModelDelegate {
+    
+    func viewModelDidChange(viewModel: SignInViewModel) {
+        print("OnChange state ->  \(viewModel.state)")
+    }
+    
+    var viewModel: SignInViewModel? {
+        didSet {
+            viewModel?.delegate = self
+        }
+    }
+    
+    let email: UITextField = {
+        let prp = UITextField()
+        prp.backgroundColor = UIColor.white
+        prp.placeholder = "Entre com seu e-mail"
+        prp.translatesAutoresizingMaskIntoConstraints = false
+        return prp
+    }()
+    
+    let password: UITextField = {
+        let prp = UITextField()
+        prp.backgroundColor = .red
+        prp.placeholder = "Entre com sua senha"
+        prp.translatesAutoresizingMaskIntoConstraints = false
+        return prp
+    }()
+    
+    lazy var send: UIButton = {
+        let prp = UIButton()
+        prp.setTitle("Entrar", for: .normal)
+        prp.setTitleColor(.black, for: .normal)
+        prp.backgroundColor = .yellow
+        prp.translatesAutoresizingMaskIntoConstraints = false
+        prp.addTarget(self, action: #selector(sendDidTap), for: .touchUpInside)
+        return prp
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+        view.addSubview(email)
+        view.addSubview(password)
+        view.addSubview(send)
+        
+        let emailConstraints = [
+            // Esquerda
+            email.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            // Direita
+            email.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            // Centro
+            email.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -100.0),
+            // Tamanho
+            email.heightAnchor.constraint(equalToConstant: 50.0)
+        ]
+        
+        let passwordConstraints = [
+            password.leadingAnchor.constraint(equalTo: email.leadingAnchor),
+            password.trailingAnchor.constraint(equalTo: email.trailingAnchor),
+            password.topAnchor.constraint(equalTo: email.bottomAnchor, constant: 8.0),
+            password.heightAnchor.constraint(equalToConstant: 50.0)
+        ]
+        
+        let sendConstraints = [
+            send.leadingAnchor.constraint(equalTo: password.leadingAnchor),
+            send.trailingAnchor.constraint(equalTo: password.trailingAnchor),
+            send.topAnchor.constraint(equalTo: password.bottomAnchor, constant: 16),
+            send.heightAnchor.constraint(equalToConstant: 60.0)
+        ]
+        
+        NSLayoutConstraint.activate(emailConstraints)
+        NSLayoutConstraint.activate(passwordConstraints)
+        NSLayoutConstraint.activate(sendConstraints)
+    }
+    
+    @objc func sendDidTap(_ sender: UIButton) {
+        viewModel?.send()
+    }
+    
+}
--- KingBurger/SignIn/ViewModel/SignInViewModel.swift
+++ KingBurger/SignIn/ViewModel/SignInViewModel.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol SignInViewModelDelegate {
+    func viewModelDidChange(viewModel: SignInViewModel)
+}
+
+class SignInViewModel {
+    
+    var delegate: SignInViewModelDelegate?
+    
+    var state: SignInState = .none {
+        didSet {
+            delegate?.viewModelDidChange(viewModel: self)
+        }
+    }
+        
+    
+    func send() {
+        state = .loading
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.state = .error("Usuário não existe")
+        }
+    }
+    
+}
