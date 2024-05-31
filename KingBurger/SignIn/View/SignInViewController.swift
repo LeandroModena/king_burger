5c409b50a4884d259af605f5e1e74373ba1d5014
Leandro <modena@outlook.com>
2024-05-31 13:27:19 -0300

    simulation error user

---
+++ KingBurger/SignIn/View/SignInViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewController.swift
+//  KingBurger
+//
+//  Created by Leandro Modena on 17/05/24.
+//
+
+import Foundation
---
+++ KingBurger/SignIn/ViewModel/SignInViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewModel.swift
+//  KingBurger
+//
+//  Created by Leandro Modena on 30/05/24.
+//
+
+import Foundation
